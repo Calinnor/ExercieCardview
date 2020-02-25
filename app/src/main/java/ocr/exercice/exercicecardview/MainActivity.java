f553c6d595995c31905ad1880f2a9c134285942f
Calinnor <frederic.hautson@gmail.com>
2020-02-25 18:54:36 +0100

    librairies importation for cardview and RecyclerView after update. Only first cardview created in layout/single_row.xml. TODO: reference ressources in java, create mcv protocol. Comming soon

---
+++ app/src/main/java/ocr/exercice/exercicecardview/MainActivity.java
@@ -0,0 +1,14 @@
+package ocr.exercice.exercicecardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
